a35d0a2f6a080232206b498c6e36803e34b05a82
https://github.com/naseerAhmadKhanDK <naseeruomian50@gmail.com>
2025-06-04 18:19:06 +0200

    add method for checking three type of parenthese

--- TestParanthesePair/Program.cs
+++ TestParanthesePair/Program.cs
@@ -2,30 +2,50 @@
 //that is each start opening have closing then it return true.
 string input="[{(a+b)-(a*t)()}]";
 
-Console.WriteLine($"The given input have balance paranthese:"+CheckParenthese(input));
-Console.WriteLine($"The given input have balance Currely Braces:" + CheckCurrelyBraces(input));
-Console.WriteLine($"The given input have balance Braket Braces:" + CheckBrakutBrace(input));
+Console.WriteLine($"The given input{input} have balance paranthese:"+CheckParenthese(input));
+Console.WriteLine($"The given input{input} have balance Currely Braces:" + CheckCurrelyBraces(input));
+Console.WriteLine($"The given input have{input} balance Braket Braces:" + CheckBrakutBrace(input));
+Console.WriteLine($"the give input have total no of :{CheckForTotalParenthese(input)} paranthes/braces/breaket brucess");
 
-static bool CheckParenthese(string input) {
-    int countOpening = 0;
-    int countClosing = 0;
+static int CheckForTotalParenthese(string input)
+{
+   // string checkParenthes = "(,),{,},[,]";
+    int count= 0;
     foreach (char c in input)
     {
-        if (c == '(')
-        {
-            countOpening++;
 
-        }
-        if (c == ')')
+        if(c == '('||c==')'||c=='{'||c=='}'||c=='['||c==']')
         {
-            countClosing++;
+            count++;
+            
         }
+        
     }
-    if (countOpening == countClosing)
-    {
-        return true;
-    }
-    else return false;
+    return count;
+    
+}
+static bool CheckParenthese(string input) {
+    int countOpening = 0;
+    int countClosing = 0;    
+
+        foreach (char c in input)
+        {
+            if (c == '(')
+            {
+                countOpening++;
+
+            }
+            if (c == ')')
+            {
+                countClosing++;
+            }
+        }
+        if (countOpening == countClosing)
+        {
+            return true;
+        }
+        else return false;
+   
 
 }
 
